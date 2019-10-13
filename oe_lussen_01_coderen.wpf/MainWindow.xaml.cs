f2ff8bb2ff44f8c2ba908943ca989c3a81f921f9
jan.de.deurwaerder@howest.be <jan.de.deurwaerder@howest.be>
2019-10-13 15:51:10 +0200

    Add project files.

---
+++ oe_lussen_01_coderen.wpf/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace oe_lussen_01_coderen.wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCodeer_Click(object sender, RoutedEventArgs e)
+        {
+            txtOnder.Text = Codeer(txtBoven.Text);
+        }
+
+        private void BtnDecodeer_Click(object sender, RoutedEventArgs e)
+        {
+            txtBoven.Text = Decodeer(txtOnder.Text);
+        }
+        string Codeer(string bron)
+        {
+            // hier komt de code
+            return "retourneer de eenvoudig gecodeerde tekst hier";
+        }
+        string Decodeer(string bron)
+        {
+            // hier komt de code
+            return "retourneer de eenvoudig gedecodeerde tekst hier";
+        }
+
+        private void BtnCodeerComplexer_Click(object sender, RoutedEventArgs e)
+        {
+            txtOnder.Text = CodeerComplexer(txtBoven.Text,"mijnSleutel");
+        }
+
+        private void BtnDecodeerComplexer_Click(object sender, RoutedEventArgs e)
+        {
+            txtBoven.Text = DecodeerComplexer(txtOnder.Text,"mijnSleutel");
+
+        }
+        string CodeerComplexer(string bron, string sleutel)
+        {
+            // hier komt de code
+            return "retourneer de complexere gecodeerde tekst hier";
+        }
+
+        string DecodeerComplexer(string bron, string sleutel)
+        {
+            // hier komt de code
+            return "retourneer de complexere gedecodeerde tekst hier";
+
+        }
+
+    }
+}
+
